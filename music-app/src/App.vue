7c0d3cd8d9ac37fcece64399072d081d5a09003d
flaviomota <flaviofilipemota@gmail.com>
2023-08-08 16:46:40 +0100

    Registration: Persist Data | Local Storage

--- music-app/src/App.vue
+++ music-app/src/App.vue
@@ -268,14 +268,25 @@
 </template>
 
 <script>
-import AppHeader from '@/components/AppHeader.vue'
-import Auth from '@/components/Auth.vue'
+import AppHeader from '@/components/AppHeader.vue';
+import Auth from '@/components/Auth.vue';
+import { mapWritableState } from 'pinia';
+import useUserStore from '@/stores/user';
+import { auth } from './includes/firebase';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
     Auth
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ['userLoggedIn'])
+  },
+  created() {
+    if (auth.currentUser) {
+      this.userLoggedIn = true;
+    }
   }
-}
+};
 </script>
