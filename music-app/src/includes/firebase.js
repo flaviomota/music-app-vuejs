ab8b66c546e5629f6a711e765e2a6e6ee5ce11a1
flaviomota <flaviofilipemota@gmail.com>
2023-08-09 13:07:04 +0100

    Upload: Drag and Drop Configs

--- music-app/src/includes/firebase.js
+++ music-app/src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAfZ94YzCds4ZOU7Wy8Px2bkbEbkod5pqo',
@@ -15,7 +16,8 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users');
 
-export { auth, db, usersCollection };
+export { auth, db, usersCollection, storage };
