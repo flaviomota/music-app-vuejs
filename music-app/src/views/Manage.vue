0c73b944ea630fe5dd29bee20f2250c6f30e678d
flaviomota <flaviofilipemota@gmail.com>
2023-08-11 10:28:59 +0100

    Upload: Cancel Feature

--- music-app/src/views/Manage.vue
+++ music-app/src/views/Manage.vue
@@ -3,7 +3,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <AppUpload />
+        <AppUpload ref="upload" />
       </div>
       <div class="col-span-2">
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
@@ -110,6 +110,10 @@ export default {
   components: {
     AppUpload
   }
+  // beforeRouteLeave(to, from, next) {
+  //   this.$refs.upload.cancelUploads();
+  //   next();
+  // }
   // eslint-disable-next-line no-unused-vars
   // beforeRouteEnter(to, from, next) {
   //   const store = useUserStore();
